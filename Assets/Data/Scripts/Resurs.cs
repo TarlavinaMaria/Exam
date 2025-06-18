e7df3f72568819914159aa0a7db993ede2a79a14
tarla <Tarlavina50@mail.ru>
2025-06-18 14:38:35 +0700

    Исправление ошибки невзятых ресурсов

--- Assets/Data/Scripts/Resurs.cs
+++ Assets/Data/Scripts/Resurs.cs
@@ -1,16 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class Resurs : MonoBehaviour
 {
+    // Переменные
     public bool IsIncludeFree { get; private set; } = false; // Флаг, указывающий, включен ли ресурс в обработку
 
     public void SetInclude() // Метод для установки флага включения ресурса в обработку
     {
         IsIncludeFree = true; // Устанавливаем флаг в true, что означает, что ресурс включен в обработку
+        StartCoroutine(AutoReset()); // Запускаем авто-сброс
     }
     public void StandartSetting() // Метод для сброса флага включения ресурса в обработку
     {
         IsIncludeFree = false; // Сбрасываем флаг включения ресурса в обработку
     }
-
+    private IEnumerator AutoReset()
+    {
+        yield return new WaitForSeconds(10f); // Ждём 10 секунд
+        IsIncludeFree = false;
+    }
 }
